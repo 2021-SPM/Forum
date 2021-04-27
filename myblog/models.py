1fec5cf31b0b4640bf49f914287123bcdd2d31c0
Pallpop <zhufeng0543@gmail.com>
2021-04-27 20:22:44 +0800

    qaq

--- myblog/models.py
+++ myblog/models.py
@@ -15,6 +15,9 @@ class Category(models.Model):
 class Tag(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return str(self.name)
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
